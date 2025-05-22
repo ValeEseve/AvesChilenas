901cb87b40881707ad7bfea68c86565d6fff2339
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-05-21 22:23:54 -0400

    Cantos agregados

--- assets/js/aves.js
+++ assets/js/aves.js
@@ -6,10 +6,11 @@ const aves = [
     habitat: "Bosques nativos de la isla Robinson Crusoe",
     dieta: "Insectos",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Archipiélago de Juan Fernández",
     riesgo: "En peligro",
-    img: "./assets./img/cachudito.jpg"
+    img: "./assets/img/cachudito.jpg",
+    canto: "./assets/audios/cachudito.mp3"
   },
   {
     nombre: "Canastero",
@@ -18,10 +19,11 @@ const aves = [
     habitat: "Matorrales y pastizales",
     dieta: "Insectos, semillas",
     dieta_value: ["insectos", "semillas"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i><i data-lucide='bean'></i>",
     zona_de_chile: "Centro y sur",
     riesgo: "Preocupación menor",
-    img: "./assets./img/canastero.jpg"
+    img: "./assets/img/canastero.jpg",
+    canto: "./assets/audios/canastero.wav"
   },
   {
     nombre: "Chiricoca",
@@ -30,10 +32,11 @@ const aves = [
     habitat: "Zonas áridas, matorrales",
     dieta: "Insectos y pequeños invertebrados",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Norte chico y centro",
     riesgo: "Preocupación menor",
-    img: "./assets./img/chiricoca.jpg"
+    img: "./assets/img/chiricoca.jpg",
+    canto: "./assets/audios/chiricoca.wav"
   },
   {
     nombre: "Choroy",
@@ -42,10 +45,11 @@ const aves = [
     habitat: "Bosques templados",
     dieta: "Semillas, frutas, brotes",
     dieta_value: ["semillas", "frutas"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bean'></i><i data-lucide='cherry'></i>",
     zona_de_chile: "Centro-sur y sur",
     riesgo: "Vulnerable",
-    img: "./assets./img/choroy.jpg"
+    img: "./assets/img/choroy.jpg",
+    canto: "./assets/audios/choroy.wav"
   },
   {
     nombre: "Churrete costero",
@@ -54,22 +58,24 @@ const aves = [
     habitat: "Costas rocosas",
     dieta: "Invertebrados marinos",
     dieta_value: ["invertebrados-marinos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='waves'></i>",
     zona_de_chile: "Desde Antofagasta a Los Lagos",
     riesgo: "Preocupación menor",
-    img: "./assets./img/churrete_costero.jpg"
+    img: "./assets/img/churrete_costero.jpg",
+    canto: "./assets/audios/churrete.mp3"
   },
   {
-    nombre: "Churrín",
+    nombre: "Churrín del Norte",
     nombre_cientifico: "Scytalopus magellanicus",
     tamano: "12",
     habitat: "Bosques densos y húmedos",
     dieta: "Insectos y larvas",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Centro-sur y sur",
     riesgo: "Preocupación menor",
-    img: "./assets./img/churrin.jpg"
+    img: "./assets/img/churrin.jpg",
+    canto: "./assets/audios/churrin.mp3"
   },
   {
     nombre: "Golondrina de mar pincoya",
@@ -78,10 +84,11 @@ const aves = [
     habitat: "Alta mar, zonas oceánicas",
     dieta: "Plancton, crustáceos pequeños",
     dieta_value: ["crustaceos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='waves'></i>",
     zona_de_chile: "Chiloé y mares circundantes",
     riesgo: "Datos insuficientes",
-    img: "./assets./img/golondrinas.jpg"
+    img: "./assets/img/golondrinas.jpg",
+    canto: "./assets/audios/golondrina.mp3"
   },
   {
     nombre: "Perdiz chilena",
@@ -90,10 +97,11 @@ const aves = [
     habitat: "Campos abiertos, matorrales",
     dieta: "Semillas, insectos",
     dieta_value: ["semillas", "insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i><i data-lucide='bean'></i>",
     zona_de_chile: "Centro y sur",
     riesgo: "Preocupación menor",
-    img: "./assets./img/perdiz_chilena.jpg"
+    img: "./assets/img/perdiz_chilena.jpg",
+    canto: "./assets/audios/perdiz.mp3"
   },
   {
     nombre: "Picaflor de Juan Fernández",
@@ -102,10 +110,11 @@ const aves = [
     habitat: "Bosques nativos y jardines",
     dieta: "Néctar e insectos",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Isla Robinson Crusoe",
     riesgo: "En peligro crítico",
-    img: "./assets./img/picaflor.png"
+    img: "./assets/img/picaflor.png",
+    canto: "./assets/audios/picaflor.mp3"
   },
   {
     nombre: "Rayadito de Mas Afuera",
@@ -114,10 +123,11 @@ const aves = [
     habitat: "Bosques húmedos",
     dieta: "Insectos",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Isla Alejandro Selkirk",
     riesgo: "En peligro crítico",
-    img: "./assets./img/rayadito.jpg"
+    img: "./assets/img/rayadito.jpg",
+    canto: "./assets/audios/rayadito.mp3"
   },
   {
     nombre: "Tapaculo",
@@ -126,10 +136,11 @@ const aves = [
     habitat: "Bosques esclerófilos y matorrales",
     dieta: "Insectos y larvas",
     dieta_value: ["insectos"],
-    dieta_icons: ,
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Centro de Chile",
     riesgo: "Casi amenazado",
-    img: "./assets./img/tapaculo.jpg"
+    img: "./assets/img/tapaculo.jpg",
+    canto: "./assets/audios/tapaculo.wav"
   },
   {
     nombre: "Turca",
@@ -137,10 +148,11 @@ const aves = [
     tamano: "23",
     habitat: "Matorrales y quebradas",
     dieta: "Insectos, lombrices",
-    dieta_value: ["insec"],
-    dieta_icons: ,
+    dieta_value: ["insectos"],
+    dieta_icons: "<i data-lucide='bug'></i>",
     zona_de_chile: "Centro y norte chico",
     riesgo: "Preocupación menor",
-    img: "./assets./img/turca.jpg"
+    img: "./assets/img/turca.jpg",
+    canto: "./assets/audios/turca.wav"
   }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,49 @@
 // Variables
-const renderBirds = document.querySelector("#render-birds")
 
 
 
-function renderBirds(filter){
-    
+function renderBirds(db){
+    const birds = document.querySelector(".render-birds")
+    birds.innerHTML = ""
+    db.forEach(ave => {
+        const card = document.createElement("article")
+        card.classList.add("card-bird")
+        card.innerHTML = `
+            <div class="card-title">
+            <img src="${ave.img}" class="card-img" alt="${ave.nombre}" />
+            <p class="card-name">${ave.nombre}</p>
+            <p class="card-c-name">${ave.nombre_cientifico}</p>
+          </div>
+          <hr />
+          <div class="card-car">
+            <div class="card-height">
+              <i data-lucide="ruler"></i>
+              <p>${ave.tamano} cm</p>
+            </div>
+            <div class="card-habitat">
+              <i data-lucide="house"></i>
+              <p>${ave.habitat}</p>
+            </div>
+            <div class="card-diet">
+              <i class="fa-solid fa-utensils card-diet-icon"></i>
+              <div class="card-diet-icons">${ave.dieta_icons}
+              </div>
+            </div>
+            <div class="card-location">
+                <i data-lucide="map-pin"></i>
+                <p>${ave.zona_de_chile}</p>
+            </div>
+            <div class="card-risk">
+                <i data-lucide="triangle-alert"></i>
+                <p>${ave.riesgo}</p>
+            </div>
+          </div>
+          <button class="card-btn"><i data-lucide="volume-2"></i></button>
+        `
+        birds.appendChild(card)
+    });
+    lucide.createIcons()
 }
+
+
+renderBirds(aves)
